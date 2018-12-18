da88a344acebdfdf6a27093466681ed0c95dcae5
Michael Saah <mike.saah@gmail.com>
2018-12-18 14:46:26 -0800

    implemented -n and -s flags

--- datagen/__main__.py
+++ datagen/__main__.py
@@ -3,23 +3,24 @@ from datagen import Datagen
 
 def main():
     description = "An extensible data generation utility"
-
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('schema', type=str,
-                        help='Path to the schema file')
+                        help='Path to the schema file, or the schema itself if -s is passed')
     parser.add_argument('-n', dest='n', metavar='N', action='store', type=int,
                         default=1,
                         help='Generate N copies of the schema')
-    parser.add_argument('-s', dest='schema_literal', action='store', type=str,
-                        metavar='schema',
-                        help='Pass the schema as an argument instead of a file')
+    parser.add_argument('-s', dest='schema_literal', action='store_true', default=False,
+                        help='If present, tells datagen to treat the input string as a schema literal, instead of as a file path.')
     parser.add_argument('-p', '--pretty', action='store_true', dest='pretty',
                         default=False,
                         help='Pretty print the output')
-
     args = parser.parse_args()
-    with open(args.schema, 'r') as f:
-        schema = f.read()
+
+    if args.schema_literal:
+        schema = args.schema
+    else:
+        with open(args.schema, 'r') as f:
+            schema = f.read()
         
     dg = Datagen()
     if args.pretty:
@@ -27,7 +28,7 @@ def main():
         data = dg(schema, args.n, native=True)
         print(json.dumps(data, indent=4))
     else:
-        print(dg(schema), args.n)
+        print(dg(schema, args.n))
 
 if __name__ == '__main__':
     main()
